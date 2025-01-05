9e43e69f036da361fb72be039d1a486450f7f158
Tran Le Trung Hieu <33504975+hieucan2805@users.noreply.github.com>
2025-01-05 23:10:19 +0700

    Update JSON Data and provider

---
+++ src/test/java/com/auto/htt/dataprovider/ORD_FLIGHT_DataProvider.java
@@ -0,0 +1,8 @@
+//package com.auto.htt.dataprovider;
+//
+//import com.auto.htt.utils.FileUtils;
+//
+//public class ORD_FLIGHT_DataProvider {
+//    private static String profileJSONPath = FileUtils.getCurrentDir() + Constants.PROFILE_JSON_DATA_PATH_ROOT;
+//
+//}
