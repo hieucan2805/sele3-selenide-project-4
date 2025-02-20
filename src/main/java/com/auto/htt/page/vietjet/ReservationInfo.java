2dbcd0486b11f00fea05a95128a88da3e4aef8ed
Tran Le Trung Hieu <33504975+hieucan2805@users.noreply.github.com>
2025-02-20 23:59:38 +0700

    Update ReservationInfo

---
+++ src/main/java/com/auto/htt/page/vietjet/ReservationInfo.java
@@ -0,0 +1,14 @@
+package com.auto.htt.page.vietjet;
+
+public class ReservationInfo extends BasePage{
+
+    //Locator
+    private final String labelFrom = "//img[@src='/static/media/departure-icon.25d3557e.svg']//following-sibling::p";
+    private final String labelTo = "//img[@src='/static/media/arrival-icon.a05c5d78.svg']//following-sibling::p";
+    private final String labelTypeAndPassenger = "//img[@src='/static/media/departure-icon.25d3557e.svg']//parent::div//preceding-sibling::p";
+    private final String labelDeparturePrice ="//p[text()='%s']//following-sibling::div/h4";
+    private final String labelReturnPrice ="//p[text()='%s']//following-sibling::div/h4";
+
+    //Method
+
+}
