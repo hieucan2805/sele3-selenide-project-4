e1109a8062e652f2b99d16e630e881d9efb1f6e9
hieu.trung.tran <hieucan1991@gmail.com>
2025-02-21 16:55:40 +0700

    update ReservationInfo.java

--- src/main/java/com/auto/htt/page/vietjet/ReservationInfo.java
+++ src/main/java/com/auto/htt/page/vietjet/ReservationInfo.java
@@ -1,7 +1,12 @@
 package com.auto.htt.page.vietjet;
 
+import com.auto.htt.utils.LocatorHelper;
+import lombok.Getter;
+
 public class ReservationInfo extends BasePage{
 
+    @Getter
+    private final LocatorHelper localeBundle = new LocatorHelper(ReservationInfo.class.getSimpleName());
     //Locator
     private final String labelFrom = "//img[@src='/static/media/departure-icon.25d3557e.svg']//following-sibling::p";
     private final String labelTo = "//img[@src='/static/media/arrival-icon.a05c5d78.svg']//following-sibling::p";
@@ -10,5 +15,10 @@ public class ReservationInfo extends BasePage{
     private final String labelReturnPrice ="//p[text()='%s']//following-sibling::div/h4";
 
     //Method
-
+    public String getText(String ele){
+        if(ele.contains("'%s'")){
+            String newXpath = localeBundle.updateLocatorWithDynamicText(labelDeparturePrice, "text.DepartureFlight");
+        }
+        return null;
+    }
 }
